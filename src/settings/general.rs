1a6b5b1e174d39224e58ac640a0941d787078eb6
kebasyaty <kebasyaty@gmail.com>
2022-06-20 13:19:31 +0300

    Update general settings

--- src/settings/general.rs
+++ src/settings/general.rs
@@ -13,8 +13,9 @@ pub static SITE_DOMAIN: &str = "www.site-name.net";
 // Max size of loaded content for Form and Json - 2.016 mb
 pub static MAX_UPLOAD_SIZE: usize = (2.016 * 1024.0 * 1024.0) as usize;
 // Security
-// http://www.miniwebtool.com/django-secret-key-generator/
-pub static SECRET_KEY: &str = "hf@$%#-ftw(ia4jualowaqlejtm17h*98pzqk18bd65um5_xnx";
+// https://passwordsgenerator.net/
+// Hint: Minimum 64 characters.
+pub static SECRET_KEY: &str = "Nd2%c7WZk!W9Qqu4AAkE_zB_6+NpYv#L2jZjHCezFP#v8As5WvUpxnEY?K-H%+J$";
 // Session
 pub static SESSION_KEY: &[u8] = SECRET_KEY.as_bytes();
 // Files
